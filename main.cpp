220c83d5770660c54919f39651b7e47a318bbcd1
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-07-19 21:12:06 +0000

    added libraries

--- main.cpp
+++ main.cpp
@@ -10,30 +10,35 @@
  * OUT OF OR IN CONNECTION WITH THE PROGRAM OR THE USE OR OTHER DEALINGS IN
  * THE PROGRAM.
  */
- 
- 
+
+
 // Define Necessary.
-#define OUTPUT_QUATERNION
+//#define OUTPUT_QUATERNION
 //#define OUTPUT_EULER
-//#define OUTPUT_ROLL_PITCH_YAW
+#define OUTPUT_ROLL_PITCH_YAW
 //#define OUTPUT_FOR_TEAPOT
 //#define OUTPUT_TEMPERATURE
- 
- 
+
+
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "mbed.h"
 #include "config.h"
 #include <stdio.h>
 
 #include "BLE.h"
- 
+#include "UARTService.h"
+
+
 #define DEG_TO_RAD(x) ( x * 0.01745329 )
 #define RAD_TO_DEG(x) ( x * 57.29578 )
- 
- 
-RawSerial pc(USBTX, USBRX);
-MPU6050 mpu(I2C_SDA0, I2C_SCL0);     // sda, scl pin
- 
+
+
+RawSerial pc(p21, p22);
+//MPU6050 mpu(I2C_SDA0, I2C_SCL0);     // sda, scl pin
+MPU6050 mpu(p9, p10);
+DigitalOut myled(p17);
+DigitalOut myled2(p19);
+
 const int FIFO_BUFFER_SIZE = 128;
 uint8_t fifoBuffer[FIFO_BUFFER_SIZE];
 uint16_t fifoCount;
@@ -43,38 +48,49 @@ uint8_t mpuIntStatus;
 const int snprintf_buffer_size = 100;
 char snprintf_buffer[snprintf_buffer_size];
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
- 
-struct Offset {
-    int16_t ax, ay, az;
-    int16_t gx, gy, gz;
-}offset = {150, -350, 1000, -110, 5, 0};    // Measured values
- 
-struct MPU6050_DmpData {
-    Quaternion q;
-    VectorFloat gravity;    // g
-    float roll, pitch, yaw;     // rad
-}dmpData;
- 
+
+
+const char* LBRACKET = "[";
+const char* RBRACKET = "]";
+const char* COMMA    = ",";
+const char* BLANK    = " ";
+const char* PERIOD   = ".";
+
+const int iAx = 0;
+const int iAy = 1;
+const int iAz = 2;
+const int iGx = 3;
+const int iGy = 4;
+const int iGz = 5;
+
+const int usDelay = 3150;   // empirical, to hold sampling to 200 Hz
+const int NFast =  1000;    // the bigger, the better (but slower)
+const int NSlow = 10000;    // ..
+const int LinesBetweenHeaders = 5;
+int LowValue[6];
+int HighValue[6];
+int Smoothed[6];
+int LowOffset[6];
+int HighOffset[6];
+int Target[6];
+int LinesOut;
+int N;
+
+
 #define CONN_INTERVAL 25  /**< connection interval 20ms; in multiples of 0.125ms. (durationInMillis * 1000) / UNIT_0_625_MS; */
 #define CONN_SUP_TIMEOUT  8000 /**< Connection supervisory timeout (6 seconds); in multiples of 0.125ms. */
 #define SLAVE_LATENCY     0
 //#define TICKER_INTERVAL   2.0f
 
 BLE   ble;
+UARTService *uart;
 
-static const char DEVICENAME[] = "TY51822r3";
+
+
+static const char DEVICENAME[] = "Calibrate";
 static volatile bool  triggerSensorPolling = false;
 
 static const char QUOTERNION_DESC[] = "quoternion";
-
-
-//const uint8_t MPU6050_adv_service_uuid[] = {
-//    0x9F,0xDF,0x32,0x83,
-//    0x90,0x49,
-//    0xCF,0x8D,
-//    0x5C,0x4D,    
-//    0x98,0xE7,0xE2,0x00,0x27,0x31
-//};
 
 static const uint8_t MPU6050_service_uuid[] = {
     0x45,0x35,0x56,0x80,0x0F,0xD8,0x5F,0xB5,0x51,0x48,0x30,0x27,0x06,0x9B,0x3F,0xD9
@@ -90,16 +106,17 @@ uint8_t quoternionPayload[sizeof(float)*4] = {0,};
 GattAttribute       quoternionAttr (BLE_UUID_DESCRIPTOR_CHAR_USER_DESC, (uint8_t *)QUOTERNION_DESC, sizeof(QUOTERNION_DESC), sizeof(QUOTERNION_DESC));
 GattAttribute       *quoternionAttrs[] = { &quoternionAttr, };
 GattCharacteristic  quoternionChar (MPU6050_dmp_Characteristic_uuid,
-                                        quoternionPayload, (sizeof(float) * 4), (sizeof(float) * 4),
-                                        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ, quoternionAttrs, 1);
+                                    quoternionPayload, (sizeof(float) * 4), (sizeof(float) * 4),
+                                    GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ, quoternionAttrs, 1);
 
 GattCharacteristic *ControllerChars[] = { &quoternionChar, };
 GattService         MPU6050DMPService(MPU6050_service_uuid, ControllerChars, sizeof(ControllerChars) / sizeof(GattCharacteristic *));
 
 
-bool Init() {
+bool Init()
+{
     pc.baud(PC_BAUDRATE);
-        
+
     mpu.initialize();
     if (mpu.testConnection()) {
         pc.printf("MPU6050 test connection passed.\n");
@@ -113,108 +130,22 @@ bool Init() {
         pc.printf("failed in MPU6050 DMP Initializing.\n");
         return false;
     }
-    mpu.setXAccelOffset(offset.ax);
-    mpu.setYAccelOffset(offset.ay);
-    mpu.setZAccelOffset(offset.az);
-    mpu.setFullScaleGyroRange(MPU6050_GYRO_FS);
-    mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS);
-    mpu.setXGyroOffsetUser(offset.gx);
-    mpu.setYGyroOffsetUser(offset.gy);
-    mpu.setZGyroOffsetUser(offset.gz);
-    mpu.setDMPEnabled(true);    // Enable DMP
-    packetSize = mpu.dmpGetFIFOPacketSize();
-    dmpReady = true;    // Enable interrupt.
+    mpu.setXAccelOffset(0);
+    mpu.setYAccelOffset(0);
+    mpu.setZAccelOffset(0);
+    mpu.setXGyroOffsetUser(0);
+    mpu.setYGyroOffsetUser(0);
+    mpu.setZGyroOffsetUser(0);
     
     pc.printf("Init finish!\n");
-    
+    myled = 1;
     return true;
 }
- 
-void bleUpdatedValue(MPU6050_DmpData dmpData)
-{
-    memcpy(quoternionPayload+sizeof(float)*0, &dmpData.q.w, sizeof(float));
-    memcpy(quoternionPayload+sizeof(float)*1, &dmpData.q.x, sizeof(float));
-    memcpy(quoternionPayload+sizeof(float)*2, &dmpData.q.y, sizeof(float));
-    memcpy(quoternionPayload+sizeof(float)*3, &dmpData.q.y, sizeof(float));
-        
-    ble.updateCharacteristicValue(quoternionChar.getValueAttribute().getHandle(), quoternionPayload, sizeof(quoternionPayload));    //Mod
-    mpu.resetFIFO();
-}
- 
-void dmpDataUpdate() {
-    // Check that this interrupt has enabled.
-    if (dmpReady == false) return;
-    
-    mpuIntStatus = mpu.getIntStatus();
-    fifoCount = mpu.getFIFOCount();
-    
-    // Check that this interrupt is a FIFO buffer overflow interrupt.
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        mpu.resetFIFO();
-        pc.printf("FIFO overflow!\n");
-        return;
-        
-    // Check that this interrupt is a Data Ready interrupt.
-    } else if (mpuIntStatus & 0x02) {
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-        
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-    #ifdef OUTPUT_QUATERNION
-        mpu.dmpGetQuaternion(&dmpData.q, fifoBuffer);
-        if ( snprintf( snprintf_buffer, snprintf_buffer_size, "Quaternion : w=%f, x=%f, y=%f, z=%f\n", dmpData.q.w, dmpData.q.x, dmpData.q.y, dmpData.q.z ) < 0 ) return;
-        pc.puts(snprintf_buffer);
-        bleUpdatedValue(dmpData);
-    #endif
-        
-    #ifdef OUTPUT_EULER
-        float euler[3];
-        mpu.dmpGetQuaternion(&dmpData.q, fifoBuffer);
-        mpu.dmpGetEuler(euler, &dmpData.q);
-        if ( snprintf( snprintf_buffer, snprintf_buffer_size, "Euler : psi=%fdeg, theta=%fdeg, phi=%fdeg\n", RAD_TO_DEG(euler[0]), RAD_TO_DEG(euler[1]), RAD_TO_DEG(euler[2]) ) < 0 ) return;
-        pc.puts(snprintf_buffer);
-    #endif
-        
-    #ifdef OUTPUT_ROLL_PITCH_YAW
-        mpu.dmpGetQuaternion(&dmpData.q, fifoBuffer);
-        mpu.dmpGetGravity(&dmpData.gravity, &dmpData.q);
-        float rollPitchYaw[3];
-        mpu.dmpGetYawPitchRoll(rollPitchYaw, &dmpData.q, &dmpData.gravity);
-        dmpData.roll = rollPitchYaw[2];
-        dmpData.pitch = rollPitchYaw[1];
-        dmpData.yaw = rollPitchYaw[0];
-        
-        if ( snprintf( snprintf_buffer, snprintf_buffer_size, "Roll:%6.2fdeg, Pitch:%6.2fdeg, Yaw:%6.2fdeg\n", RAD_TO_DEG(dmpData.roll), RAD_TO_DEG(dmpData.pitch), RAD_TO_DEG(dmpData.yaw) ) < 0 ) return;
-        pc.printf(snprintf_buffer);
-    #endif
-        
-    #ifdef OUTPUT_FOR_TEAPOT
-        teapotPacket[2] = fifoBuffer[0];
-        teapotPacket[3] = fifoBuffer[1];
-        teapotPacket[4] = fifoBuffer[4];        
-        teapotPacket[5] = fifoBuffer[5];
-        teapotPacket[6] = fifoBuffer[8];
-        teapotPacket[7] = fifoBuffer[9];
-        teapotPacket[8] = fifoBuffer[12];
-        teapotPacket[9] = fifoBuffer[13];
-        for (uint8_t i = 0; i < 14; i++) {
-            pc.putc(teapotPacket[i]);
-        }
-    #endif
-        
-    #ifdef OUTPUT_TEMPERATURE
-        float temp = mpu.getTemperature() / 340.0 + 36.53;
-        if ( snprintf( snprintf_buffer, snprintf_buffer_size, "Temp:%4.1fdeg\n", temp ) < 0 ) return;
-        pc.puts(snprintf_buffer);
-    #endif
-        
-        pc.printf("\n");
-    }
-}
+
 
 void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)    // Mod
 {
-    
+
     //DEBUG("Disconnected handle %u, reason %u\n", handle, reason);
     //DEBUG("Restarting the advertising process\n\r");
 
@@ -232,13 +163,13 @@ void connectionCallback(const Gap::ConnectionCallbackParams_t *params)
     /*  CFG_GAP_CONNECTION_MIN_INTERVAL_MS / CFG_GAP_CONNECTION_MAX_INTERVAL_MSを  */
     /*  直接編集すること                                                             */
     /******************************************************************************/
-        //Gap::Handle_t handle;
-        Gap::ConnectionParams_t gap_conn_params;
-        gap_conn_params.minConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
-        gap_conn_params.maxConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
-        gap_conn_params.connectionSupervisionTimeout = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_SUP_TIMEOUT);
-        gap_conn_params.slaveLatency                 = SLAVE_LATENCY;
-        
+    //Gap::Handle_t handle;
+    Gap::ConnectionParams_t gap_conn_params;
+    gap_conn_params.minConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
+    gap_conn_params.maxConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
+    gap_conn_params.connectionSupervisionTimeout = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_SUP_TIMEOUT);
+    gap_conn_params.slaveLatency                 = SLAVE_LATENCY;
+
     if (ble.updateConnectionParams(params->handle, &gap_conn_params) != BLE_ERROR_NONE) {
         //DEBUG("failed to update connection paramter\r\n");
     }
@@ -247,12 +178,13 @@ void connectionCallback(const Gap::ConnectionCallbackParams_t *params)
 void timeoutCallback(const Gap::TimeoutSource_t source)
 {
     //DEBUG("TimeOut\n\r");
-    //DEBUG("Restarting the advertising process\n\r");    
+    //DEBUG("Restarting the advertising process\n\r");
 
     ble.startAdvertising();
 }
 
-void bleInit(){
+void bleInit()
+{
     ble.init();
     ble.onDisconnection(disconnectionCallback);
     ble.onConnection(connectionCallback);
@@ -261,14 +193,14 @@ void bleInit(){
 
     /* setup device name */
     ble.setDeviceName((const uint8_t *)DEVICENAME);
-    
+
     /* setup advertising */
     ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED  | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
     ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (const uint8_t *)DEVICENAME, sizeof(DEVICENAME));
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
-                                    (const uint8_t *)MPU6050_service_uuid, sizeof(MPU6050_service_uuid));
-                                    //(const uint8_t *)MPU6050_adv_service_uuid, sizeof(MPU6050_adv_service_uuid));
+                                     (const uint8_t *)MPU6050_service_uuid, sizeof(MPU6050_service_uuid));
+    //(const uint8_t *)MPU6050_adv_service_uuid, sizeof(MPU6050_adv_service_uuid));
 
     ble.setAdvertisingInterval(160); /* 100ms; in multiples of 0.625ms. */
     ble.startAdvertising();
@@ -276,32 +208,206 @@ void bleInit(){
     ble.addService(MPU6050DMPService);
 }
 
-void periodicCallback(void)
+void sendString(const char *str)
 {
-    //oneSecondLed = !oneSecondLed; /* Do blinky on LED1 while we're waiting for BLE events */
-
-    /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
-     * heavy-weight sensor polling from the main thread. */
-    triggerSensorPolling = true;
+    uart->writeString(str);
+}
+void sendInt(const int num)
+{
+    char str[12];
+    sprintf(str, "%d", num);
+    uart->writeString(str);
 }
 
+void ForceHeader()
+{
+    LinesOut = 99;
+}
 
-int main() {
-    MBED_ASSERT(Init() == true);
-    
-    Ticker ticker;
-    ticker.attach(periodicCallback, 0.1f);
-    
-    bleInit();
-    
-    while(true) {
-        if (triggerSensorPolling && ble.getGapState().connected) {
-            triggerSensorPolling = false;
-            dmpDataUpdate();
-        } else {
-            ble.waitForEvent();
-        }
-
+void GetSmoothed()
+{
+    int16_t RawValue[6];
+    int i;
+    long Sums[6];
+    for (i = iAx; i <= iGz; i++) {
+        Sums[i] = 0;
     }
-}
+//    unsigned long Start = micros();
+
+    for (i = 1; i <= N; i++) {
+        // get sums
+        mpu.getMotion6(&RawValue[iAx], &RawValue[iAy], &RawValue[iAz],
+                       &RawValue[iGx], &RawValue[iGy], &RawValue[iGz]);
+        //if ((i % 500) == 0) {sendString(PERIOD);}
+        wait_us(usDelay);
+        for (int j = iAx; j <= iGz; j++) {
+            //sendInt(RawValue[j]);
+            Sums[j] = Sums[j] + RawValue[j]; }
+    } // get sums
+//    unsigned long usForN = micros() - Start;
+//    Serial.print(" reading at ");
+//    Serial.print(1000000/((usForN+N/2)/N));
+//    Serial.println(" Hz");
+    for (i = iAx; i <= iGz; i++) {
+        Smoothed[i] = (Sums[i] + N/2) / N ;
+    }
+} // GetSmoothed
+
+void SetOffsets(int TheOffsets[6])
+{
+    mpu.setXAccelOffset(TheOffsets[iAx]);
+    mpu.setYAccelOffset(TheOffsets[iAy]);
+    mpu.setZAccelOffset(TheOffsets[iAz]);
+    mpu.setXGyroOffsetUser(TheOffsets[iGx]);
+    mpu.setYGyroOffsetUser(TheOffsets[iGy]);
+    mpu.setZGyroOffsetUser(TheOffsets[iGz]);
  
+    
+} // SetOffsets
+
+
+void ShowProgress()
+{
+    for (int i = iAx; i <= iGz; i++) {
+        sendInt(LowOffset[i]);
+        sendString("\n");
+        sendInt(HighOffset[i]);
+        sendString("\n");
+        wait(1);
+    }  
+    
+} // ShowProgress
+
+void SetAveraging(int NewN)
+{
+    N = NewN;
+    //sendString("averaging ");
+    //sendInt(N);
+    //sendString(" readings each time\n");
+} // SetAveraging
+
+
+
+void PullBracketsIn()
+{
+    bool AllBracketsNarrow;
+    bool StillWorking;
+    int NewOffset[6];
+
+    //sendString("\nclosing in:");
+    AllBracketsNarrow = false;
+    ForceHeader();
+    StillWorking = true;
+    while (StillWorking) {
+        StillWorking = false;
+        if (AllBracketsNarrow && (N == NFast)) {
+            SetAveraging(NSlow);
+        } else {
+            AllBracketsNarrow = true;    // tentative
+        }
+        for (int i = iAx; i <= iGz; i++) {
+            if (HighOffset[i] <= (LowOffset[i]+1)) {
+                NewOffset[i] = LowOffset[i];
+            } else {
+                // binary search
+                StillWorking = true;
+                NewOffset[i] = (LowOffset[i] + HighOffset[i]) / 2;
+                if (HighOffset[i] > (LowOffset[i] + 10)) {
+                    AllBracketsNarrow = false;
+                }
+            } // binary search
+        }
+        SetOffsets(NewOffset);
+        GetSmoothed();
+        for (int i = iAx; i <= iGz; i++) {
+            // closing in
+            if (Smoothed[i] > Target[i]) {
+                // use lower half
+                HighOffset[i] = NewOffset[i];
+                HighValue[i] = Smoothed[i];
+            } // use lower half
+            else {
+                // use upper half
+                LowOffset[i] = NewOffset[i];
+                LowValue[i] = Smoothed[i];
+            } // use upper half
+        } // closing in
+        //ShowProgress();
+    } // still working
+
+} // PullBracketsIn
+
+
+void PullBracketsOut()
+{
+    bool Done = false;
+    int NextLowOffset[6];
+    int NextHighOffset[6];
+
+    //sendString("expanding:");
+    ForceHeader();
+
+    while (!Done) {
+        Done = true;
+        SetOffsets(LowOffset);
+        GetSmoothed();
+        for (int i = iAx; i <= iGz; i++) {
+            // got low values
+            LowValue[i] = Smoothed[i];
+            if (LowValue[i] >= Target[i]) {
+                Done = false;
+                NextLowOffset[i] = LowOffset[i] - 1000;
+            } else {
+                NextLowOffset[i] = LowOffset[i];
+            }
+        } // got low values
+
+        SetOffsets(HighOffset);
+        GetSmoothed();
+        for (int i = iAx; i <= iGz; i++) {
+            // got high values
+            HighValue[i] = Smoothed[i];
+            if (HighValue[i] <= Target[i]) {
+                Done = false;
+                NextHighOffset[i] = HighOffset[i] + 1000;
+            } else {
+                NextHighOffset[i] = HighOffset[i];
+            }
+        } // got high values
+        //ShowProgress();
+        for (int i = iAx; i <= iGz; i++) {
+            LowOffset[i] = NextLowOffset[i];   // had to wait until ShowProgress done
+            HighOffset[i] = NextHighOffset[i]; // ..
+        }
+    } // keep going
+} // PullBracketsOut
+
+
+
+int main()
+{
+    MBED_ASSERT(Init() == true);
+    bleInit();
+    uart = new UARTService(ble);
+    wait(3);
+
+
+    for (int i = iAx; i <= iGz; i++) {
+        // set targets and initial guesses
+        Target[i] = 0; // must fix for ZAccel
+        HighOffset[i] = 0;
+        LowOffset[i] = 0;
+    } // set targets and initial guesses
+    Target[iAz] = 16384;
+    SetAveraging(NFast);
+
+    PullBracketsOut();
+    PullBracketsIn();
+    
+    ShowProgress();
+    wait(10);
+    
+    myled2 = 1;
+    myled = 0;
+
+}
